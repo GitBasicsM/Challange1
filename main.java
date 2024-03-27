d7636abedbc6e75f59d1b3f38746f40a6c57c254
Zbroadbent2 <zachbroadbent04@gmail.com>
2024-03-27 19:39:29 -0400

    Printed login'

--- main.java
+++ main.java
@@ -1,15 +1,19 @@
 package Challange1;
+
+import java.util.Scanner;
+
 public class main {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Please login using your Quinnipiac email.");
+        String email = scanner.nextLine();
 
+        System.out.println("Please login using your Quinnipiac password.");
+        String password = scanner.nextLine();
 
-
-public static void main(String[] args) {
-
-
-
-}
-
+        scanner.close(); 
+    }
 }
 
