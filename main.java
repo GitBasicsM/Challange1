bab830a35535c1a84b4e7a05baaeb3b054c03f0d
dbhargav <99452015+dbhargavQU@users.noreply.github.com>
2024-04-10 14:19:14 -0400

    updated main

--- main.java
+++ main.java
@@ -3,22 +3,23 @@ package Challange1;
 import java.util.Scanner;
 
 public class main {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static UserSession userSession = new UserSession();
+    private static ItemInventory itemInventory = new ItemInventory();
 
-
-public static void main(String[] args) {
-
-    System.out.println("__      __       .__                                __________      ___.                  __  ._.\n" + //
-            "/  \\    /  \\ ____ |  |   ____  ____   _____   ____   \\______   \\ ____\\_ |__   ____ _____ _/  |_| |\n" + //
-            "\\   \\/\\/   // __ \\|  | _/ ___\\/  _ \\ /     \\_/ __ \\   |    |  _//  _ \\| __ \\_/ ___\\\\__  \\\\   __\\ |\n" + //
-            " \\        /\\  ___/|  |_\\  \\__(  <_> )  Y Y  \\  ___/   |    |   (  <_> ) \\_\\ \\  \\___ / __ \\|  |  \\|\n" + //
-            "  \\__/\\  /  \\___  >____/\\___  >____/|__|_|  /\\___  >  |______  /\\____/|___  /\\___  >____  /__|  __\n" + //
-            "       \\/       \\/          \\/            \\/     \\/          \\/           \\/     \\/     \\/      \\/Quinnipiacs item selling program");
-
-
-
-            
-
+    public static void main(String[] args) {
+        System.out.println(" __      __       .__                                __________      ___.                  __          \r\n" + //
+                        "/  \\    /  \\ ____ |  |   ____  ____   _____   ____   \\______   \\ ____\\_ |__   ____ _____ _/  |_  ______\r\n" + //
+                        "\\   \\/\\/   // __ \\|  | _/ ___\\/  _ \\ /     \\_/ __ \\   |    |  _//  _ \\| __ \\_/ ___\\\\__  \\\\   __\\/  ___/\r\n" + //
+                        " \\        /\\  ___/|  |_\\  \\__(  <_> )  Y Y  \\  ___/   |    |   (  <_> ) \\_\\ \\  \\___ / __ \\|  |  \\___ \\ \r\n" + //
+                        "  \\__/\\  /  \\___  >____/\\___  >____/|__|_|  /\\___  >  |______  /\\____/|___  /\\___  >____  /__| /____  >\r\n" + //
+                        "       \\/       \\/          \\/            \\/     \\/          \\/           \\/     \\/     \\/          \\/ ");
+        while (!userSession.isLoggedIn()) {
+            if (!userSession.login(scanner)) {
+                return; // Exit the application if login fails
+            }
+        }
+        Menu.showMainMenu(scanner, userSession, itemInventory);
+    }
 }
 
-
-}
